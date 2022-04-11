8343964aed2a9f4b1d51fcd930f2a1a030d516a1
obenenya <enenyaobinna@gmail.com>
2022-04-11 22:04:55 +0100

    Made changes to the login interface

--- portal.php
+++ portal.php
@@ -1,4 +1,4 @@
-<?php
+<?php 
 require_once 'php/includes/function.php';
 require_once 'php/includes/pdo_connect.php';
 // *** Validate request to login to this site.
@@ -7,24 +7,21 @@ require_once 'php/includes/pdo_connect.php';
 if($_SERVER['REQUEST_METHOD'] ==='POST'){
     $user_name =  htmlspecialchars(trim(post_data('user_name')));
     $password = htmlspecialchars(trim(post_data('password')));
-    $role1 =  htmlspecialchars(trim(post_data('role')));
+    $role =  htmlspecialchars(trim(post_data('role')));
 
     validateUsername($user_name, $errors);
     validatePassword($password, $errors);
-    validateRole($role1, $errors);
+    validateRole($role, $errors);
     if(!empty($errors)){
 			$status = 'Oh Snap! Ensure required fields are filled correctly';
-    }
+    }else{
    
       $statement = $conn->prepare("SELECT user_id,  user_name, role, secure_pass FROM  user_pass WHERE user_name = :user_name  AND role = :role" );
         $statement->bindValue('user_name', $user_name);
-        $statement->bindValue('role', $role1);
+        $statement->bindValue('role', $role);
         $statement->execute();
         $count = $statement->rowCount();
-    //$results = $statement->fetchALL(PDO::FETCH_ASSOC);
         
-    
-    
 //new
      if ($count > 0){
       while ($row = $statement->fetch(PDO::FETCH_ASSOC)) {
@@ -55,158 +52,156 @@ if($_SERVER['REQUEST_METHOD'] ==='POST'){
       $status = "No user found.";
      }
      $statement = null;
+  }
 }
 
 
 ?>
-
 <!DOCTYPE html>
 <html lang="en">
-  <head>
-  <meta charset="UTF-8">
-  <meta http-equiv="X-UA-Compatible" content="IE=edge">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>RCCG Hom Ejigbo | Portal</title>
-    <?php include("php/meta.php"); ?>
-    
-    <link href="css/custom.css" rel="stylesheet">
+<head>
+	<meta charset="UTF-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+		<title>RCCG HOM EJIGBO | Sign Up</title>
+	<?php include("php/meta.php"); ?>
+        <?php include("php/links.php"); ?>
+		<link href="css/custom.css" rel="stylesheet">
+	</head>
 
 
-    <link rel="stylesheet" href="bootstrap/css/bootstrap.min.css">
+	<body class="no-trans    ">
 
+		<!-- scrollToTop -->
+		<!-- ================ -->
+		<div class="scrollToTop circle"><i class="icon-up-open-big"></i></div>
+		
+		<!-- page wrapper start -->
+		<!-- ================ -->
+		<div class="page-wrapper">
+		
+		<?php include("php/includes/layouts/header_admin.php"); ?>
+		
+			<!-- breadcrumb start -->
+			<!-- ================ -->
+			<div class="breadcrumb-container">
+				<div class="container">
+					<ol class="breadcrumb">
+						<li><i class="fa fa-home pr-10"></i><a href="Index">Portal</a></li>
+                       
+						<li class="active">Please Sign In</li>
+					</ol>
+				</div>
+			</div>
+			<!-- breadcrumb end -->
 
-
-    <style>
- 
-body {
-  display: -ms-flexbox;
-  display: flex;
-  -ms-flex-align: center;
-  align-items: center;
-  padding-top: 40px;
-  padding-bottom: 40px;
-  background-color: #fff !important;
-}
-
-h1{
-  font-family: serif;
-    font-size: 30px;
-    color: #29272778;
-}
-
-.form-signin {
-  width: 100%;
-  max-width: 330px;
-  padding: 15px;
-  margin: auto;
-}
-.form-signin .checkbox {
-  font-weight: 400;
-}
-.form-signin .form-control {
-  position: relative;
-  box-sizing: border-box;
-  height: auto;
-  padding: 10px;
-  font-size: 16px;
-}
-.form-signin .form-control:focus {
-  z-index: 2;
-}
-.form-signin input[type="email"] {
-  margin-bottom: -1px;
-  border-bottom-right-radius: 0;
-  border-bottom-left-radius: 0;
-}
-.form-signin input[type="password"] {
-  margin-bottom: 10px;
-  border-top-left-radius: 0;
-  border-top-right-radius: 0;
-}
-
-      .bd-placeholder-img {
-        font-size: 1.125rem;
-        text-anchor: middle;
-        -webkit-user-select: none;
-        -moz-user-select: none;
-        -ms-user-select: none;
-        user-select: none;
-      }
-
-      #select{
-        margin-top: -10px;
-        border-top-left-radius: 0;
-       border-top-right-radius: 0;
-       color: #ab9d9d;
-          }
-
-
-      @media (min-width: 768px) {
-        .bd-placeholder-img-lg {
-          font-size: 3.5rem;
-        }
-      }
-    </style>
-     <!-- Custom styles for this template -->
-    <link href="signin.css" rel="stylesheet">
-  
-  </head>
-
-  <body class="text-center">
-
-  <form action="" method="post" class="form-signin" id="portal-form">
-  <img class="mb-4"  src="images/logo.png" alt="Houseofmercy" width="130" height="70">
-  <h1 >Please sign in</h1>
-                  <?php if(isset($status)) : ?>
-										<p class='text-center text-uppercase' style='color:red'><?php echo $status; ?></p>
-
-									<?php elseif(isset($msg)) : ?>
-										<p class='text-center text-uppercase' style='color:green'><?php echo $msg; ?></p>
+			<!-- main-container start -->
+			<!-- ================ -->
+			<div class="main-container dark-translucent-bg" style="background-image:url('images/pattern-8.png');">
+				<div class="container">
+					<div class="row">
+						<!-- main start -->
+						<!-- ================ -->
+						<div class="main object-non-visible" data-animation-effect="fadeInUpSmall" data-effect-delay="100">
+							<div class="form-block center-block p-30 light-gray-bg border-clear">
+								<h2 class="title text-center">Sign In</h2>
+								<?php
+									if(isset($status)){
+										echo "<p class='text-center text-uppercase' style='color:red'>{$status}</p>";
+									}
+									else if(isset($msg)){
+										echo "<p class='text-center text-uppercase' style='color:green'>{$msg}</p>";
+									}
+									else{
+										echo "";
+									}
+								?>
+								<form action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>" method="post" id ="portal-form" class="form-horizontal" role="form">
+                                	
+                                	<div class="form-group has-feedback">
+										<label for="user_name" class="col-sm-3 control-label">User Name</label>
+										<div class="col-sm-8">
+											<input type="text" class="form-control" id="user_name" name="user_name" >
+											<i class="fa fa-user form-control-feedback"></i>
+             								<span id="name_status"></span>
+             								<div class="invalid-feedback"><span class="text-danger small"><?php echo $errors['user_name'] ?? ''?></span>
+											</div>
+										</div>
+									</div>
+                                     <div class="form-group has-feedback">
+										<label for="password" class="col-sm-3 control-label">Password</label>
+										<div class="col-sm-8">
+											<input type="password" class="form-control" id="password" name="password"  required>
+											<i class="fa fa-lock form-control-feedback"></i>
+											<span id="pswd_status"></span>
+											<div class="invalid-feedback">
+											    <span class="text-danger small"><?php echo $errors['password'] ?? ''?></span>
+											</div>
+										</div>
+									</div>
+                                    <div class="form-group has-feedback">
+										<label for="role" class="col-sm-3 control-label">Role</label>
+										<div class="col-sm-8">
+                                        <select name="role" class="form-control" id="role">
+                                            <option value="">Select role</option>
+                                            <option value="Admin">Admin</option>
+                                            <option value="Counselor">Counselor</option>
+                                            <option value="Pastorate">Pastorate</option>
+                                        </select>
+											<div class="invalid-feedback">
+												<span class="text-danger small"><?php echo $errors['role'] ?? ''?></span>
+											</div>
+										</div>
+									</div>
 									
-									<?php else : ?>
-										
-									<?php endif; ?> 
-  <label for="inputEmail" class="sr-only">Email address</label>
-  <input type="text" class="form-control" name="user_name" id="uname" placeholder="User Name" required autofocus>
-  <label for="inputPassword" class="sr-only">Password</label>
-  <input type="password" name="password" class="form-control" id="pwd" placeholder="Password" required>
-  <label for="role" class="sr-only">Role</label>
-    <select name="role" class="form-control" id="select">
-                            <option value="">Select role</option>
-                                  <option value="Admin">Admin</option>
-                                  <option value="Counselor">Counselor</option>
-                                  <option value="Pastorate">Pastorate</option>
-                        </select>
-  <button class="btn btn-lg btn-primary btn-block" name="login" type="submit">Sign in</button>
-  <p class="mt-5 mb-3 text-muted">&copy;obinna 2021</p>
-</form>
+									<div class="form-group">
+										<div class="col-sm-offset-3 col-sm-8">
+											<button type="submit" class="btn btn-group btn-default btn-animated">Submit <i class="fa fa-check"></i></button>
+										</div>
+									</div>
+								</form>
+							</div>
+						</div>
+						<!-- main end -->
+					</div>
+				</div>
+			</div>
+			<!-- main-container end -->
+			
+<?php include("php/includes/layouts/footer_admin.php"); ?>
+			
+		</div>
+		<!-- page-wrapper end -->
 
+		<!-- JavaScript files placed at the end of the document so the pages load faster -->
+		<!-- ================================================== -->
+		<!-- Jquery and Bootstap core js files -->
+		<script type="text/javascript" src="plugins/jquery.min.js"></script>
+		<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
+		<script type="text/javascript" src="bootstrap/js/bootstrap.min.js"></script>
+		<!-- Modernizr javascript -->
+		<script type="text/javascript" src="plugins/modernizr.js"></script>
+		<!-- Magnific Popup javascript -->
+		<script type="text/javascript" src="plugins/magnific-popup/jquery.magnific-popup.min.js"></script>
+		<!-- Appear javascript -->
+		<script type="text/javascript" src="plugins/waypoints/jquery.waypoints.min.js"></script>
+		<!-- Count To javascript -->
+		<script type="text/javascript" src="plugins/jquery.countTo.js"></script>
+		<!-- Parallax javascript -->
+		<script src="plugins/jquery.parallax-1.1.3.js"></script>
+		<!-- Contact form -->
+		<script src="plugins/jquery.validate.js"></script>
+		<!-- Owl carousel javascript -->
+		<script type="text/javascript" src="plugins/owl-carousel/owl.carousel.js"></script>
+		<!-- SmoothScroll javascript -->
+		<script type="text/javascript" src="plugins/jquery.browser.js"></script>
+		<script type="text/javascript" src="plugins/SmoothScroll.js"></script>
+		<!-- Initialization of Plugins -->
+		<script type="text/javascript" src="js/template.js"></script>
+		<!-- Custom Scripts -->
+		<script type="text/javascript" src="js/custom.js"></script>
 
+		   
 
-
-  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
-    <script type="text/javascript" src="plugins/jquery.min.js"></script>
-    
-    <!-- Modernizr javascript -->
-    <script type="text/javascript" src="plugins/modernizr.js"></script>
-    <!-- Magnific Popup javascript -->
-    <script type="text/javascript" src="plugins/magnific-popup/jquery.magnific-popup.min.js"></script>
-    <!-- Appear javascript -->
-    <script type="text/javascript" src="plugins/waypoints/jquery.waypoints.min.js"></script>
-    <!-- Count To javascript -->
-    <script type="text/javascript" src="plugins/jquery.countTo.js"></script>
-    <!-- Parallax javascript -->
-    <script src="plugins/jquery.parallax-1.1.3.js"></script>
-    <!-- Contact form -->
-    <script src="plugins/jquery.validate.js"></script>
-    <!-- Owl carousel javascript -->
-    <script type="text/javascript" src="plugins/owl-carousel/owl.carousel.js"></script>
-    <!-- SmoothScroll javascript -->
-    <script type="text/javascript" src="plugins/jquery.browser.js"></script>
-    <script type="text/javascript" src="plugins/SmoothScroll.js"></script>
-    <!-- Initialization of Plugins -->
-    <script type="text/javascript" src="js/template.js"></script>
-    <!-- Custom Scripts -->
-    <script type="text/javascript" src="js/custom.js"></script>
-</body>
+	</body>
 </html>
