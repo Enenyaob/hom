ec15f4e468efc679caa4810df7735c9c757e37a3
obenenya <enenyaobinna@gmail.com>
2022-04-14 02:46:05 +0100

    created send mail nodule

---
+++ php/contactForm.php
@@ -0,0 +1,42 @@
+<?php
+  if($_SERVER['REQUEST_METHOD'] ==='POST'){
+  $name = htmlspecialchars(trim($_POST['name2']));
+  $email = htmlspecialchars(trim($_POST['email2']));
+  $subject = htmlspecialchars(trim($_POST['subject2']));
+  $message = htmlspecialchars(trim($_POST['message2']));
+  header('Content-Type: application/json');
+
+  
+if (empty($name) || empty($email) || empty($message)){
+  header("Location:javascript://history.go(-1)");
+  # code...
+}elseif(!filter_var($email, FILTER_VALIDATE_EMAIL)){
+  header("Location:javascript://history.go(-1)");
+}else{
+  $header = "From: " . $email;
+  $message2 ="You have received a message from " . $name . ".\n\n" . $message;
+
+  
+  $myMail = "obinnaenenya@gmail.com";
+ 
+
+      if (mail ($myMail, $subject, $message2, $header)) {
+         header("Location:../index?mailsend");
+        exit();
+    }else{
+      echo 'Something went wrong';
+       header("Location:../index?error");
+    }
+  }    
+
+}else{
+  echo 'Error!';
+}
+
+
+?>
+
+
+
+
+
